bd0cdb5cc82f10e50dd613dcded9ce7afd3df250
Urbi <158612655+UR295@users.noreply.github.com>
2024-09-10 10:24:21 +0530

    Update phn.py

--- phn.py
+++ phn.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 # Define the main page layout
 def main_page():
-    st.title("Welcome!")
-    
+    st.title("Welcome !")
+    st.write("Help !")
     # Create a logo button
-    if st.button("📞 Contact Us:"):
+    if st.button("📞 Contact Us"):
         phone_number_page()
 
 # Define the phone number page
